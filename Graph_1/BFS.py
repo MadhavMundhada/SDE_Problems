cd0bad3225e25f33da0c43730574298783585549
MadhavMundhada <50283834+MadhavMundhada@users.noreply.github.com>
2021-03-07 08:32:19 +0530

    Update BFS.py

--- Graph_1/BFS.py
+++ Graph_1/BFS.py
@@ -1,1 +1,38 @@
 
+from collections import defaultdict
+
+def bfs(s):
+    visited=[False]*y
+    q=[]
+    q.append(s)
+
+    visited[s]=True
+    #print(q)
+    while q:
+        s=q.pop(0)
+        print(s,end=" ")
+        for i in graph[s]:
+            
+            if visited[i]==False:
+                q.append(i)
+                visited[i]=True
+
+
+graph=defaultdict(list)
+y, k = map(int,input().split())
+
+for __ in range(k):
+
+    
+    t, m =map(int,input().split())
+    graph[t].append(m)
+    graph[m].append(t) 
+    
+print(graph)
+bfs(int(input()))
+
+
+
+
+
+        
